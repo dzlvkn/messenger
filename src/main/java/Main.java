a655c6641fea5f6c1661a712bb002cb19d951861
dzlvkn <makhmud0vaa@yandex.ru>
2025-11-23 19:19:48 +0300

    Добавить класс Пользователь

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println(Dialog.getChatWithMom());
+        System.out.println(Dialog.getChatWithSister());
+        System.out.println(Dialog.getChatWithFriend1());
+        System.out.println(Dialog.getChatWithFriend2());
+
+
     }
 }
